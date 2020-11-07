f9e201301fd4aa274ac8f250b0e41d9e30b9af25
jahidulsaeid <jahdiul.saeid@gmail.com>
2020-11-07 12:54:03 +0600

    user login validation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,11 +2,50 @@ const User = require("../../models/User");
 const { SECRET_KEY } = require("../../config");
 const { UserInputError } = require("apollo-server");
 
-const { validateRegisterInput } = require("../../util/validators");
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../util/validators");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
+function generateToken(user) {
+  jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+}
+
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+      if (!valid){
+        throw new UserInputError("Errors", { errors });
+      }
+      const user = await User.findOne({ username });
+      if (!user) {
+        errors.genaral = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.genaral = "Wrong Crendentials";
+        throw new UserInputError("Wrong Crendentials", { errors });
+      }
+      const token = generateToken(user);
+      return {
+        
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
     async register(
       _,
       { registerInput: { username, email, password, confirmPassword } },
@@ -42,15 +81,7 @@ module.exports = {
       });
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res);
 
       return {
         ...res._doc,
